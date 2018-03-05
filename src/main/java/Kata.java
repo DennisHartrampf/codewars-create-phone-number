c250c6a336e30087a13140e59c0c5a22414b7041
Dennis Hartrampf <dennis.hartrampf@postcon.de>
2018-03-05 13:48:03 +0100

    Solution

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,5 +1,20 @@
 public class Kata {
     public static String createPhoneNumber(int[] numbers) {
-        // Your code here!
+        StringBuilder phoneNumber = new StringBuilder("(");
+        for (int i = 0; i < numbers.length; i++) {
+            switch (i) {
+                case 2:
+                    phoneNumber.append(numbers[i]);
+                    phoneNumber.append(") ");
+                    break;
+                case 5:
+                    phoneNumber.append(numbers[i]);
+                    phoneNumber.append("-");
+                    break;
+                default:
+                    phoneNumber.append(numbers[i]);
+            }
+        }
+        return phoneNumber.toString();
     }
 }
--- src/test/java/PhoneExampleTests.java
+++ src/test/java/PhoneExampleTests.java
@@ -6,4 +6,9 @@ public class PhoneExampleTests {
     public void tests() {
         assertEquals("(123) 456-7890", Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
     }
+
+    @Test
+    public void tests2() {
+        assertEquals("(000) 000-0000", Kata.createPhoneNumber(new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}));
+    }
 }
